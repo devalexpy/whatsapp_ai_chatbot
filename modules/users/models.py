c091b8e0ac1a4adf54d7dfe6b688fde8f3737327
Alex Arroyo <alexarroyo9912@gmail.com>
2025-12-27 22:22:23 -0500

    Add Settings class to User model to define collection name as 'users' for database consistency.

--- modules/users/models.py
+++ modules/users/models.py
@@ -10,3 +10,6 @@ class User(Document):
     picture: str
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     updated_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+
+    class Settings:
+        name = "users"
